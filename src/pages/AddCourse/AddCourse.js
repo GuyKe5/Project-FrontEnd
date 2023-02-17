4402cd2d4f4b2b0a49c5fea32b7e63b2a92eac4b
GuyKe5 <guykeren2002@gmail.com>
2023-02-17 11:01:27 +0200

    .

--- src/pages/AddCourse/AddCourse.js
+++ src/pages/AddCourse/AddCourse.js
@@ -2,8 +2,37 @@ import {useState,useEffect} from "react"
 import "../Login/Login.css";
 function AddCourse(props){
     const [formData,setFormData] = useState({name:'',description:''});
-     function handleSubmit(){
-        const response = fetch()
+    const [msg,setMsg] = useState("")
+    async function handleSubmit(event){
+        event.preventDefault();
+        try{
+        const response = await fetch ('https://localhost:7162/api/Course/AddCourse', {
+            method: 'PUT',
+            headers: {
+                'accept': '*/*',
+                'Content-Type': 'application/json'
+            },
+            // body: '\n{\n"name":"f"\n}',
+            body: JSON.stringify({
+                'name': formData.name,
+                'description' : formData.description
+            })
+        });
+        console.log(response)
+        if(response.ok){
+            
+            console.log("succes adding course")
+
+        }
+
+
+
+    }
+    catch{
+        console.log("error in addCourse")
+    }
+
+        
     }
     function handleChange(event) {
         const { name, value } = event.target;
@@ -23,6 +52,7 @@ function AddCourse(props){
                 : תיאור 
                 </div>
                 <button type="submit"> submit</button>
+                <div style={{ color: 'red'  }}>{msg}</div>
             </form>
 
                 
